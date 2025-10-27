9ad8e8378a28c99102596477697b933eec96be55
naidneelttil <naidneelttil@protonmail.com>
2025-10-27 13:12:31 -0400

    added boilerplate for the ?role command

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ client.once(Events.ClientReady, (readyClient) => {
 
 // Log in to Discord with your client's token
 client.login(token);
+
+
+client.on('messageCreate', message => {
+	if (message.content === '?role') {
+		// if you are an admin or a chat mod, you should be able to change roles
+		const modRole = message.guild.roles.cache.find(role => role.name === 'Administrator' || role.name === 'Chat Moderator');
+		if (message.member.roles.cache.has(modRole.id)) {
+			message.reply('You have access to this command!');
+			// Place role command actions here
+		}
+		else {
+			message.reply('Only moderators can use this command.');
+		}
+	}
+});
